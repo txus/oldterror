18c716b4ca8818a89d53b79c4b071271a00580eb
Josep M. Bach <josep.m.bach@gmail.com>
2011-12-03 17:45:05 +0100

    print all the instructions

---
+++ opcode.h
@@ -0,0 +1,10 @@
+#ifndef __opcode_h__
+#define __opcode_h__
+enum {
+  PUSH_INT,
+  ADD,
+  RET
+};
+
+typedef unsigned char byte;
+#endif
--- vm.c
+++ vm.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <assert.h>
 #include "dbg.h"
+#include "opcode.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
+  byte instructions[] = {
+    PUSH_INT, 2,
+    PUSH_INT, 2,
+    ADD,
+    RET
+  };
+
+  // Instruction pointer
+  byte *ip = instructions;
+  int count = sizeof(instructions) / sizeof(byte);
+  int i;
+
+  // Print all the instructions
+  for(i = 0; i < count; i++) {
+    printf("%i", *(ip + i));
+  }
+
   return 0;
 }
