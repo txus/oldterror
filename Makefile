824f741ac40e28ba1aec232098b4f2d04458810c
Josep M. Bach <josep.m.bach@gmail.com>
2011-12-03 17:32:03 +0100

    first commit

---
+++ Makefile
@@ -0,0 +1,7 @@
+CFLAGS=-Wall -g
+
+all: vm
+
+clean:
+	rm -f vm
+	rm -fR *.dSYM
---
+++ vm.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <assert.h>
+#include "dbg.h"
+
+int main(int argc, char *argv[])
+{
+  return 0;
+}
