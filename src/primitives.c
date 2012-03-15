d1d87fc3fff65d99fd95d8f35f0f8e5abb24b6cf
Josep M. Bach <josep.m.bach@gmail.com>
2012-03-15 17:43:34 +0100

    Check arithmetic types in primitives

--- src/primitives.c
+++ src/primitives.c
@@ -1,41 +1,62 @@
+#include <assert.h>
 #include "primitives.h"
+
+void
+check_type(Object* object, int type) {
+  assert(object->type == type);
+}
 
 Object*
 Primitive_add(Object *left, Object *right)
 {
-  int num_left, num_right;
-  num_left  = left->value.integer;
-  num_right = right->value.integer;
+  check_type(left, tInteger);
+  check_type(right, tInteger);
 
-  return Integer_new(num_left + num_right);
+  int a, b;
+
+  a = left->value.integer;
+  b = right->value.integer;
+
+  return Integer_new(a + b);
 }
 
 Object*
 Primitive_sub(Object *left, Object *right)
 {
-  int num_left, num_right;
-  num_left  = left->value.integer;
-  num_right = right->value.integer;
+  check_type(left, tInteger);
+  check_type(right, tInteger);
 
-  return Integer_new(num_left - num_right);
+  int a, b;
+  a = left->value.integer;
+  b = right->value.integer;
+
+  return Integer_new(a - b);
 }
 
 Object*
 Primitive_mul(Object *left, Object *right)
 {
-  int num_left, num_right;
-  num_left  = left->value.integer;
-  num_right = right->value.integer;
+  check_type(left, tInteger);
+  check_type(right, tInteger);
 
-  return Integer_new(num_left * num_right);
+  int a, b;
+  a = left->value.integer;
+  b = right->value.integer;
+
+  return Integer_new(a * b);
 }
 
 Object*
 Primitive_div(Object *left, Object *right)
 {
-  int num_left, num_right;
-  num_left  = left->value.integer;
-  num_right = right->value.integer;
+  check_type(left, tInteger);
+  check_type(right, tInteger);
 
-  return Integer_new(num_left / num_right);
+  int a, b;
+  a = left->value.integer;
+  b = right->value.integer;
+
+  assert(b != 0);
+
+  return Integer_new(a / b);
 }
--- src/primitives.h
+++ src/primitives.h
@@ -1,7 +1,10 @@
 #ifndef __primitives_h__
 #define __primitives_h__
 
+#include <assert.h>
 #include "object.h"
+
+void check_type(Object *object, int type);
 
 Object* Primitive_add(Object *, Object *);
 Object* Primitive_sub(Object *, Object *);
