34f21c1a6889d1e40b74bf74d5ce10b143f1d679
Josep M. Bach <josep.m.bach@gmail.com>
2012-06-09 20:36:06 +0200

    Add slot

---
+++ src/terror/slot.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <terror/slot.h>
+#include <terror/dbg.h>
+
+Slot* Slot_new(bstring name) {
+  Slot *slot = calloc(1, sizeof(Slot));
+  check_mem(slot);
+
+  slot->name = name;
+  slot->value.method = NULL;
+  slot->value.object = NULL;
+  return slot;
+
+error:
+  return NULL;
+}
+
+void Slot_destroy(Slot *slot) {
+  if (slot->name) bdestroy(slot->name);
+  if (slot->value.method) {
+    VMMethod_destroy(slot->value.method);
+  }
+  free(slot);
+}
---
+++ src/terror/slot.h
@@ -0,0 +1,18 @@
+#ifndef _terror__slot_h_
+#define _terror__slot_h_
+
+#include <terror/vmmethod.h>
+#include <terror/bstrlib.h>
+
+typedef struct {
+  bstring name;
+  union {
+    VMMethod *method;
+    Object *object;
+  } value;
+} Slot;
+
+Slot* Slot_new(bstring name);
+void Slot_destroy(Slot *slot);
+
+#endif
